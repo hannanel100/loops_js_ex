a4b1d70b1c19b6e573947ccbb13d3616c5c56516
Hannanel Gershinsky <hannanel100@gmail.com>
2019-05-13 22:03:04 +0300

    3rd loop to print even numbers from 1 to n using while

--- loops.js
+++ loops.js
@@ -1,15 +1,24 @@
-var n = 20;
-/*prints all numbers from 1 to n with while
+var n = 100;
+/*1. prints all numbers from 1 to n with while
 var count = 1;
 while(count <= n){
     console.log(count);
     count++;
 }
 */
-/*prints numbers from n to 1 with while
+/*2. prints numbers from n to 1 with while
 var count = 0;
 while(count < n){
     console.log(n - count);
+    count++;
+}
+*/
+/*3.print even numbers from 1 to 100 using while
+var count = 1;
+while (count <= n) {
+    if (count % 2 == 0) {
+        console.log(count);
+    }
     count++;
 }
 */
