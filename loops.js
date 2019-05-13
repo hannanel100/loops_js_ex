bcd133317cbf195e62063ed7000d623b2553acdb
Hannanel Gershinsky <hannanel100@gmail.com>
2019-05-13 21:54:36 +0300

    added second loop which prints numbers from n to 1 using while

--- loops.js
+++ loops.js
@@ -1,7 +1,15 @@
 var n = 20;
-
+/*prints all numbers from 1 to n with while
 var count = 1;
 while(count <= n){
     console.log(count);
     count++;
 }
+*/
+/*prints numbers from n to 1 with while
+var count = 0;
+while(count < n){
+    console.log(n - count);
+    count++;
+}
+*/
